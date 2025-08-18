6e3af0a47b49e9e7a8a03bac14d4dfcbcf692da0
ferchuqui <fernando.farji@gmail.com>
2025-08-18 19:49:57 -0300

    nuevos cambios

--- ejemplos/php/mssql.php
+++
@@ -1,6 +0,0 @@
-<?php
-	if  ($conn = mssql_connect( "localhost", '', '' ) )
-		echo "anda ok";
-	else
-		echo "ni para atras";
-?>
--- js/funciones.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-function popup(url) {
-	day = new Date();
-	id = day.getTime();
-	eval("page" + id + " = window.open(url, '" + id + "', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=508,height=180');");
-}
